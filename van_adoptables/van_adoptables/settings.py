a21df81ea0aa8751b263f741f5dcb31758b8d171
Min-Ji Han <minji-h@Min-Jis-iMac.local>
2019-08-25 23:34:55 -0700

    added listings and organizations apps

--- van_adoptables/van_adoptables/settings.py
+++ van_adoptables/van_adoptables/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'organizations.apps.OrganizationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- van_adoptables/van_adoptables/urls.py
+++ van_adoptables/van_adoptables/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
+    path('adoptables/', include('listings.urls')),
+    path('admin/', admin.site.urls)
 ]

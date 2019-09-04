7b3eb685d2dc32c9dee5ac45034942924bf8655a
Min-Ji Han <minjihan@msn.com>
2019-09-03 21:10:53 -0700

    Added fields to organizations model, ran initial migrations.

--- van_adoptables/organizations/models.py
+++ van_adoptables/organizations/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 class Organization(models.Model):
-    pass
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    email = models.EmailField()
+    phone = models.TextField(max_length=20)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+
+    def __str__(self):
+        return self.name
